5bf072b01d5ab6ddd6a4e02da0d204772f602f0c
AntoineduFresne <antoine@du-fresne.ch>
2026-04-05 21:13:04 +0200

    update

--- ARA/QuickSort.lean
+++ ARA/QuickSort.lean
@@ -24,6 +24,12 @@ private noncomputable abbrev qs_branch (L : List ℕ) (i : Fin L.length) : PMF (
   (QuickSort_A ((L.eraseIdx i).filter (· ≥ L[i]))).bind fun S2 =>
   PMF.pure (S1 ++ [L[i]] ++ S2)
 
+private noncomputable abbrev qs_branch' (L : List ℕ) (i : Fin L.length) : PMF (List ℕ) :=
+   (QuickSort_A ((L.eraseIdx i).filter (· < L[i]))).bind fun S1 =>
+   (QuickSort_A ((L.eraseIdx i).filter (· ≥ L[i]))).bind fun S2 =>
+   PMF.pure (S1 ++ [L[i]] ++ S2)
+
+lemma qs_br_eq: qs_branch = qs_branch' := by rfl
 /-- Two sorted ℕ-permutations are equal. -/
 lemma eq_of_sortedLE_perm {l1 l2 : List ℕ}
     (h1 : l1.SortedLE) (h2 : l2.SortedLE) (hp : l1.Perm l2) : l1 = l2 :=
